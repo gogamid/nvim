dd3a339e2941d11b51471ca565fda441994cf9ec
gogamid <gamidliimron@gmail.com>
2023-08-31 18:08:01 +0200

    changes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+***REMOVED***
+
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      -- transparent_mode = true,
+  ***REMOVED***
+***REMOVED***
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
