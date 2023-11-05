6a5d5d19ce1f859836d091f109641dee643844d6
gogamid <gamidliimron@gmail.com>
2023-11-05 12:00:37 +0100

    remove unnecessary files

--- init.lua
+++ init.lua
@@ -1,8 +1,1 @@
-if vim.g.vscode then
-  print("You are using Nvim vscode extension")
-  require("config.vscode.init")
-else
-  local isPersonal = vim.fn.isdirectory("C:/Users/dtwj6af") ~= 0
-  print(isPersonal)
-  require("config.lazy")
-end
+require("config.lazy")
