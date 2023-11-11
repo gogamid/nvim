1705efe47abc491adfbd91ded765a039facba5be
gogamid <36050790+gogamid@users.noreply.github.com>
2023-11-11 18:21:35 +0100

    python setup

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,15 @@
+***REMOVED***
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
+      ***REMOVED***
+    ***REMOVED***
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,6 @@
+***REMOVED***
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+***REMOVED***
+***REMOVED***
