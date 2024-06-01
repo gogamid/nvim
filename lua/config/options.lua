5af22b6712d5f507a890d4e3934cc8e37f041e27
gogamid <36050790+gogamid@users.noreply.github.com>
2024-06-01 12:09:47 +0200

    update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,17 +4,12 @@
 local opt = vim.opt
 opt.termguicolors = true
 
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-opt.foldlevelstart = 99
-opt.foldnestmax = 10 -- deepest fold is 10 levels
-opt.foldenable = false -- don't fold by default
-opt.foldlevel = 1
 opt.relativenumber = false
 
+-- python
 vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
-
 local home = os.getenv("HOME")
 vim.g.python3_host_prog = home .. "/.pyenv/versions/pynvim/bin/python"
+
 vim.g.copilot_status = false
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -40,17 +40,4 @@ return {
       },
     },
   },
-  {
-    "nvim-telescope/telescope-file-browser.nvim",
-    keys = {
-      {
-        "<leader>sB",
-        ":Telescope file_browser path=%:p:h<cr>",
-        desc = "Browse files",
-      },
-    },
-    config = function()
-      require("telescope").load_extension("file_browser")
-    end,
-  },
 }
