a1ea648b4763a52d97dd3df84b75c291d0396e9b
gogamid <36050790+gogamid@users.noreply.github.com>
2024-10-15 21:02:19 +0200

    better telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,12 @@
+local actions = require("telescope.actions")
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+      },
+    },
     opts = {
       defaults = {
         layout_strategy = "vertical",
@@ -11,8 +17,25 @@ return {
           "filename_first",
         },
       },
+      pickers = {
+        buffers = {
+          mappings = {
+            n = {
+              ["<C-x>"] = actions.delete_buffer,
+            },
+            i = {
+              ["<C-x>"] = actions.delete_buffer,
+            },
+          },
+        },
+      },
     },
     keys = {
+      {
+        "<leader>/",
+        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Grep (root dir)",
+      },
       {
         "<leader>fP",
         function()
@@ -21,5 +44,25 @@ return {
         desc = "Find Plugin File",
       },
     },
+    config = function(_, opts)
+      local tele = require("telescope")
+
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      opts.extensions = {
+        live_grep_args = {
+          auto_quoting = true, -- enable/disable auto-quoting
+          -- define mappings, e.g.
+          mappings = { -- extend mappings
+            i = {
+              ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+        },
+      }
+      tele.setup(opts)
+      tele.load_extension("live_grep_args")
+    end,
   },
 }
