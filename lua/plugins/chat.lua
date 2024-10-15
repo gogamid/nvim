b9f7ad7721d0d15434226aac1cde6cde7076a7ce
gogamid <36050790+gogamid@users.noreply.github.com>
2024-10-15 21:02:32 +0200

    better chat

---
+++ lua/plugins/chat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      window = {
+        layout = "float", -- 'vertical', 'horizontal', 'float', 'replace'
+        width = 0.8,
+      },
+    },
+  },
+}
