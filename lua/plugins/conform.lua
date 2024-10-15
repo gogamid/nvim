5e4726ce447caee8623f7162bb89998f715ccac0
gogamid <36050790+gogamid@users.noreply.github.com>
2024-10-15 21:02:06 +0200

    less timeout on format in go

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,16 +2,21 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      default_format_opts = {
+        lsp_format = "fallback",
+        -- async = true,
+        timeout_ms = 500,
+      },
       formatters_by_ft = {
         proto = { "buf" },
       },
       formatters = {
-        -- goimports = {
-        --   prepend_args = { "-local", "dev.azure.com/schwarzit/lidl.wawi-core/" },
-        -- },
+        goimports = {
+          prepend_args = { "-local", "dev.azure.com/schwarzit/lidl.wawi-core/" },
+        },
         formatters_by_ft = {
           go = function()
-            return { "gofumpt" } -- removing goimports
+            return { "goimports", "gofumpt" } -- removing goimports if needed
           end,
         },
       },
