cde25844b80326fe86860b933495fe343b874ebf
gogamid <36050790+gogamid@users.noreply.github.com>
2024-11-12 08:54:42 +0100

    telescoop grep root is git root

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,24 @@
+local function find_git_root()
+  -- Use the current buffer's path as the starting point for the git search
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir
+  local cwd = vim.fn.getcwd()
+  -- If the buffer is not associated with a file, return nil
+  if current_file == "" then
+    current_dir = cwd
+  else
+    -- Extract the directory from the current file's path
+    current_dir = vim.fn.fnamemodify(current_file, ":h")
+  end
+
+  -- Find the Git root directory from the current file's path
+  local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+  if vim.v.shell_error ~= 0 then
+    print("Not a git repository. Searching on current working directory")
+    return cwd
+  end
+  return git_root
+end
 local actions = require("telescope.actions")
 return {
   {
@@ -33,8 +54,17 @@ return {
     keys = {
       {
         "<leader>/",
-        "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-        desc = "Grep (root dir)",
+        function()
+          local git_root = find_git_root()
+          if git_root then
+            require("telescope").extensions.live_grep_args.live_grep_args({
+              search_dirs = { git_root },
+            })
+          else
+            require("telescope").extensions.live_grep_args.live_grep_args()
+          end
+        end,
+        desc = "Grep (git root)",
       },
       {
         "<leader>fP",
@@ -57,6 +87,8 @@ return {
             i = {
               ["<C-k>"] = lga_actions.quote_prompt(),
               ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              -- freeze the current list and start a fuzzy search in the frozen list
+              ["<C-space>"] = actions.to_fuzzy_refine,
             },
           },
         },
