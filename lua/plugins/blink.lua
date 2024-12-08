f627bc8a1a3aa5059791306287f143c7d9b50b23
gogamid <36050790+gogamid@users.noreply.github.com>
2024-12-08 20:22:31 +0100

    C-y yes for blink anstatt enter

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<C-y>"] = { "accept", "fallback" },
+        ["<CR>"] = {},
+      },
+    },
+    -- version = "v0.7.0",
+  },
+}
