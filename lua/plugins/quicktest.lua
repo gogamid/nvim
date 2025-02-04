e963e23b07ed2b2d48d0e28662f5c2ea36337b2d
gogamid <36050790+gogamid@users.noreply.github.com>
2025-02-04 23:40:21 +0100

    update quicktest

--- lua/plugins/quicktest.lua
+++ lua/plugins/quicktest.lua
@@ -1,12 +1,46 @@
 return {
   "quolpr/quicktest.nvim",
-  config = function()
-    local qt = require("quicktest")
-
-    qt.setup({
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+  },
+  keys = {
+    { "<leader>tl", ":QuicktestRunLine<CR>", desc = "[T]est Run [L]line" },
+    { "<leader>tf", ":QuicktestRunFile<CR>", desc = "[T]est Run [F]ile" },
+    { "<leader>td", ":QuicktestRunDir<CR>", desc = "[T]est Run [D]ir" },
+    { "<leader>ta", ":QuicktestRunAll<CR>", desc = "[T]est Run [A]ll" },
+    {
+      "<leader>tp",
+      function()
+        require("quicktest").run_previous()
+      end,
+      desc = "[T]est Run [P]revious",
+    },
+    {
+      "<leader>tt",
+      function()
+        require("quicktest").toggle_win("popup")
+      end,
+      desc = "[T]est [T]oggle Window",
+    },
+    {
+      "<leader>ts",
+      function()
+        local qt = require("quicktest")
+        qt.cancel_current_run()
+      end,
+      desc = "[T]est [C]ancel Current Run",
+    },
+  },
+  config = function(_, opts)
+    require("quicktest").setup({
       -- Choose your adapter, here all supported adapters are listed
       adapters = {
-        require("quicktest.adapters.golang")({}),
+        require("quicktest.adapters.golang")({
+          additional_args = function(_)
+            return { "-tags=manual_test" }
+          end,
+        }),
         require("quicktest.adapters.vitest")({}),
       },
       -- split or popup mode, when argument not specified
@@ -35,75 +69,4 @@ return {
       use_builtin_colorizer = true,
     })
   end,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-  },
-  keys = {
-    {
-      "<leader>tl",
-      function()
-        local qt = require("quicktest") -- current_win_mode return currently opened panel, split or popup
-        qt.run_line()
-        -- You can force open split or popup like this:
-        -- qt.run_line('split')
-        -- qt.run_line('popup')
-      end,
-      desc = "[T]est Run [L]line",
-    },
-    {
-      "<leader>tf",
-      function()
-        local qt = require("quicktest")
-
-        qt.run_file()
-      end,
-      desc = "[T]est Run [F]ile",
-    },
-    {
-      "<leader>td",
-      function()
-        local qt = require("quicktest")
-
-        qt.run_dir()
-      end,
-      desc = "[T]est Run [D]ir",
-    },
-    {
-      "<leader>ta",
-      function()
-        local qt = require("quicktest")
-
-        qt.run_all()
-      end,
-      desc = "[T]est Run [A]ll",
-    },
-    {
-      "<leader>tp",
-      function()
-        local qt = require("quicktest")
-
-        qt.run_previous()
-      end,
-      desc = "[T]est Run [P]revious",
-    },
-    {
-      "<leader>tt",
-      function()
-        local qt = require("quicktest")
-
-        qt.toggle_win("popup")
-      end,
-      desc = "[T]est [T]oggle Window",
-    },
-    {
-      "<leader>tc",
-      function()
-        local qt = require("quicktest")
-
-        qt.cancel_current_run()
-      end,
-      desc = "[T]est [C]ancel Current Run",
-    },
-  },
 }
