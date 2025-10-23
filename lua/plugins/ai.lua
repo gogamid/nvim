af1b857b72923793465990428545a6a2157ad28b
gogamid <36050790+gogamid@users.noreply.github.com>
2025-10-24 00:39:41 +0200

    up

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,7 +3,7 @@ vim.g.last_session_id = nil
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    enabled = false,
+    enabled = true,
     keys = {
       { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
       { "<leader>aM", ":CopilotChatModels<CR>", desc = "CopilotChat Models", mode = { "n" } },
@@ -89,7 +89,7 @@ return {
   },
   {
     "supermaven-inc/supermaven-nvim",
-    enabled = false,
+    enabled = true,
     keys = {
       {
         "<leader>aS",
@@ -368,94 +368,38 @@ return {
   },
   {
     "yetone/avante.nvim",
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
+    enabeld = false,
     build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
       or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
-    ---@module 'avante'
-    ---@type avante.Config
     opts = {
-      provider = "copilot",
       selection = {
         hint_display = "none",
       },
       behaviour = {
-        -- auto_set_keymaps = false,
+        auto_set_highlight_group = false,
+        auto_set_keymaps = true,
       },
-      acp_providers = {
-        ["opencode"] = {
-          command = "opencode",
-          args = { "acp" },
-        },
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
+      provider = "copilot",
+      selector = {
+        provider = "snacks",
       },
     },
-  },
-  cmd = {
-    "AvanteAsk",
-    "AvanteBuild",
-    "AvanteChat",
-    "AvanteClear",
-    "AvanteEdit",
-    "AvanteFocus",
-    "AvanteHistory",
-    "AvanteModels",
-    "AvanteRefresh",
-    "AvanteShowRepoMap",
-    "AvanteStop",
-    "AvanteSwitchProvider",
-    "AvanteToggle",
-  },
-  keys = {
-    { "<leader>aa", "<cmd>AvanteAsk<CR>", desc = "Ask Avante" },
-    { "<leader>ac", "<cmd>AvanteChat<CR>", desc = "Chat with Avante" },
-    { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "Avante Edit Settings" },
-    { "<leader>af", "<cmd>AvanteFocus<CR>", desc = "Avante Focus Mode" },
-    { "<leader>ah", "<cmd>AvanteHistory<CR>", desc = "Avante History" },
-    { "<leader>am", "<cmd>AvanteModels<CR>", desc = "Select Avante Model" },
-    { "<leader>an", "<cmd>AvanteChatNew<CR>", desc = "New Avante Chat" },
-    { "<leader>ap", "<cmd>AvanteSwitchProvider<CR>", desc = "Switch Avante Provider" },
-    { "<leader>ar", "<cmd>AvanteRefresh<CR>", desc = "Refresh Avante" },
-    { "<leader>as", "<cmd>AvanteStop<CR>", desc = "Stop Avante" },
-    { "<leader>at", "<cmd>AvanteToggle<CR>", desc = "Toggle Avante" },
+    cmd = {
+      "AvanteAsk",
+      "AvanteBuild",
+      "AvanteChat",
+      "AvanteClear",
+      "AvanteEdit",
+      "AvanteFocus",
+      "AvanteHistory",
+      "AvanteModels",
+      "AvanteRefresh",
+      "AvanteShowRepoMap",
+      "AvanteStop",
+      "AvanteSwitchProvider",
+      "AvanteToggle",
+    },
   },
 }
