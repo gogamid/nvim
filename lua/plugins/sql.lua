1ff00ee96f091b3068aa8abe787aa0cbd8ef29bf
gogamid <36050790+gogamid@users.noreply.github.com>
2025-11-15 10:45:13 +0100

    up

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -1,11 +1,16 @@
 return {
-  {
-    "tpope/vim-dadbod",
-  },
-  {
-    "kristijanhusak/vim-dadbod-completion",
-  },
-  {
-    "kristijanhusak/vim-dadbod-ui",
-  },
+  { "tpope/vim-dadbod" },
+  { "kristijanhusak/vim-dadbod-completion" },
+  { "kristijanhusak/vim-dadbod-ui" },
+  { dir = "~/personal/nvim-plugins/sql-ghosty.nvim", opts = {
+
+    highlight_group = "conceal",
+  } },
+  -- {
+  --   "pmouraguedes/sql-ghosty.nvim",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   opts = {},
+  -- },
 }
