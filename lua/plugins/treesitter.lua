66e8c1e4939ad12aa3e1b8874c6fe9d1e0d1ef2d
gogamid <36050790+gogamid@users.noreply.github.com>
2025-11-18 23:44:20 +0100

    k

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -55,17 +55,5 @@ return {
         },
       })
     end,
-    keys = {
-      -- { "<leader>uI", ":InspectTree<CR>", desc = "Inspect Tree" },
-      -- { "<leader>uE", ":EditQuery<CR>", desc = "Edit Query" },
-      {
-        "<leader>uI",
-        function()
-          vim.cmd("InspectTree")
-          vim.cmd("EditQuery")
-        end,
-        desc = "Inspect and Edit Query",
-      },
-    },
   },
 }
