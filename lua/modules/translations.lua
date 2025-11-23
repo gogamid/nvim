7c792db8238a6eaaf7007ffb5b4a1c7211e3db1a
gogamid <36050790+gogamid@users.noreply.github.com>
2025-11-23 09:17:59 +0100

    working for full vue

--- lua/modules/translations.lua
+++ lua/modules/translations.lua
@@ -21,16 +21,15 @@ local go_translation_query = [[
   (#any-of? @ident "translations" "translation")) 
 ]]
 
-local query_by_ft = {
+local query_by_lang = {
   typescript = ts_translation_query,
-  vue = ts_translation_query,
   go = go_translation_query,
 }
 
-local lang_by_ft = {
-  typescript = "typescript",
-  vue = "typescript",
-  go = "go",
+local active = {
+  typescript = true,
+  vue = true,
+  go = true,
 }
 
 local highlight = "Conceal"
@@ -69,31 +68,10 @@ local function get_translation_value(key, translations)
   end
 end
 
-local function on_win(_, win, buf, top, bottom)
-  local filetype = vim.bo[buf].filetype
-
-  if query_by_ft[filetype] == nil then
-    return false
-  end
-
-  vim.api.nvim_buf_clear_namespace(buf, ns, top, bottom)
-
-  local parser = vim.treesitter.get_parser(buf, lang_by_ft[filetype])
-  if not parser then
-    return false
-  end
-
-  local parsed = parser:parse({ top, bottom + 1 })
-  if not parsed then
-    return false
-  end
-
-  -- Load translations once per window render, todo: maybe autocommand when we open buffer?
-  local translations = load_translations(vim.fn.findfile("en_DEV.json"))
-
-  local query = vim.treesitter.query.parse(lang_by_ft[filetype], query_by_ft[filetype])
-
-  for _, node in pairs(parsed) do
+---@param trees table<integer, TSTree>
+local function handle_trees(trees, lang, buf, top, bottom, translations)
+  local query = vim.treesitter.query.parse(lang, query_by_lang[lang])
+  for _, node in pairs(trees) do
     for _, match, _ in query:iter_matches(node:root(), buf, top, bottom + 1) do
       for id, nodes in pairs(match) do
         local capture_name = query.captures[id]
@@ -112,25 +90,45 @@ local function on_win(_, win, buf, top, bottom)
       end
     end
   end
+end
 
-  -- for _, match, _ in query:iter_matches(tree:root(), buf, top, bottom + 1) do
-  --   for id, nodes in pairs(match) do
-  --     local capture_name = query.captures[id]
-  --     if capture_name == "translation.key" then
-  --       local key_node = nodes[1]
-  --       local key_text = vim.treesitter.get_node_text(key_node, buf)
-  --       local start_row, start_col, end_row, end_col = key_node:range()
-  --
-  --       local value = get_translation_value(key_text, translations)
-  --
-  --       -- Set virtual text showing actual value after the key
-  --       vim.api.nvim_buf_set_extmark(buf, ns, end_row, end_col + 1, {
-  --         virt_text = { { " → " .. value, highlight } },
-  --         virt_text_pos = "inline",
-  --       })
-  --     end
-  --   end
-  -- end
+local function on_win(_, win, buf, top, bottom)
+  local filetype = vim.bo[buf].filetype
+  if not active[filetype] then
+    return false
+  end
+
+  vim.api.nvim_buf_clear_namespace(buf, ns, top, bottom)
+
+  local lt = vim.treesitter.get_parser(buf)
+  if not lt then
+    return false
+  end
+
+  --- @type table<string, vim.treesitter.LanguageTree>
+  local ltrees = {}
+  if query_by_lang[lt:lang()] ~= nil then
+    ltrees[lt:lang()] = lt
+  else
+    for lang, ltree in pairs(lt:children()) do
+      if query_by_lang[lang] ~= nil then
+        ltrees[lang] = ltree
+      end
+    end
+  end
+  if not next(ltrees) then
+    return false
+  end
+
+  local translations = load_translations(vim.fn.findfile("en_DEV.json"))
+
+  for lang, ltree in pairs(ltrees) do
+    local tstrees = ltree:parse({ top, bottom + 1 })
+    if tstrees then
+      handle_trees(tstrees, lang, buf, top, bottom + 1, translations)
+    end
+  end
+
   return true
 end
 
