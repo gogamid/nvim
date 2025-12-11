50217bc03cd7bdb40de0edc004555f6b9c5e70bb
gogamid <36050790+gogamid@users.noreply.github.com>
2025-12-11 21:55:38 +0100

    runners

---
+++ lua/overseer/template/run_lua.lua
@@ -0,0 +1,27 @@
+return {
+  name = "run_lua",
+  builder = function()
+    local file = vim.api.nvim_buf_get_name(0)
+    local cmd = { "nvim", "-l" }
+    return {
+      cmd = cmd,
+      args = { file },
+      metadata = { run_cmd = string.format("%s %s %s", cmd[1], cmd[2], file) },
+      components = {
+        { "on_output_quickfix", open = true },
+        "default",
+      },
+      default_component_params = {
+        errorformat = ""
+          .. [[%*[^:]:\ Lua\ chunk:\ %f:%l:%m,]]
+          .. [[%t%n:\ Error\ while\ creating\ lua\ chunk:\ %f:%l:\ %m,]]
+          .. [[%t%n:\ Error\ while\ calling\ lua\ chunk:\ %f:%l:\ %m,]]
+          .. [[%f:%l:%m,]]
+          .. [[%-G[Process exited%.%#,]],
+      },
+    }
+  end,
+  condition = {
+    filetype = { "lua" },
+  },
+}
