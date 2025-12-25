2989560d3e8fed901f2630a06e7a9c872dcb81bc
gogamid <36050790+gogamid@users.noreply.github.com>
2025-12-25 23:48:41 +0100

    up

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -113,25 +113,6 @@ return {
             end,
           },
         },
-        -- "location"
-        -- - `branch` (git branch)
-        -- - `buffers` (shows currently available buffers)
-        -- - `diagnostics` (diagnostics count from your preferred source)
-        -- - `diff` (git diff status)
-        -- - `encoding` (file encoding)
-        -- - `fileformat` (file format)
-        -- - `filename`
-        -- - `filesize`
-        -- - `filetype`
-        -- - `hostname`
-        -- - `location` (location in file in line:column format)
-        -- - `mode` (vim mode)
-        -- - `progress` (%progress in file)
-        -- - `searchcount` (number of search matches when hlsearch is active)
-        -- - `selectioncount` (number of selected characters or lines)
-        -- - `tabs` (shows currently available tabs)
-        -- - `windows` (shows currently available windows)
-        -- - `lsp_status` (shows active LSPs in the current buffer and a progress spinner)
         lualine_x = { supermaven, "overseer", overseer_status, "diagnostics", "diff", "progress" },
         lualine_y = {},
         lualine_z = {},
