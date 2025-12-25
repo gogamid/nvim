fff8a626115024f70c69d7f8fa812a25ab27989f
gogamid <36050790+gogamid@users.noreply.github.com>
2025-12-25 23:44:24 +0100

    up

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,7 +1,7 @@
 local leet_arg = "leetcode.nvim"
 return {
   "kawre/leetcode.nvim",
-  enabled = false,
+  enabled = true,
   lazy = leet_arg ~= vim.fn.argv()[1],
   cmd = "Leet",
   opts = {
@@ -28,17 +28,17 @@ return {
     "MunifTanjim/nui.nvim",
   },
   keys = {
-    { "<leader>lq", ":Leet tabs<cr>", desc = "Leetcode Tabs" },
-    { "<leader>lm", ":Leet menu<cr>", desc = "Leetcode Menu" },
-    { "<leader>lc", ":Leet console<cr>", desc = "Leetcode Console" },
-    { "<leader>li", ":Leet info<cr>", desc = "Leetcode Info" },
-    { "<leader>ll", ":Leet lang<cr>", desc = "Leetcode Lang" },
-    { "<leader>ld", ":Leet desc<cr>", desc = "Leetcode Desc" },
-    { "<leader>lr", ":Leet run<cr>", desc = "Leetcode Run" },
-    { "<leader>ls", ":Leet submit<cr>", desc = "Leetcode Submit" },
-    { "<leader>ly", ":Leet yank<cr>", desc = "Leetcode Yank" },
-    { "<leader>lp", ":Leet list<cr>", desc = "Leetcode List" },
-    { "<leader>lx", ":Leet reset<cr>", desc = "Leetcode Reset" },
-    { "<leader>lh", ":Leet last_submit<cr>", desc = "Leetcode Last Submit" },
+    { ft = leet_arg, "<leader>lq", ":Leet tabs<cr>", desc = "Leetcode Tabs" },
+    { ft = leet_arg, "<leader>lm", ":Leet menu<cr>", desc = "Leetcode Menu" },
+    { ft = leet_arg, "<leader>lc", ":Leet console<cr>", desc = "Leetcode Console" },
+    { ft = leet_arg, "<leader>li", ":Leet info<cr>", desc = "Leetcode Info" },
+    { ft = leet_arg, "<leader>ll", ":Leet lang<cr>", desc = "Leetcode Lang" },
+    { ft = leet_arg, "<leader>ld", ":Leet desc<cr>", desc = "Leetcode Desc" },
+    { ft = leet_arg, "<leader>lr", ":Leet run<cr>", desc = "Leetcode Run" },
+    { ft = leet_arg, "<leader>ls", ":Leet submit<cr>", desc = "Leetcode Submit" },
+    { ft = leet_arg, "<leader>ly", ":Leet yank<cr>", desc = "Leetcode Yank" },
+    { ft = leet_arg, "<leader>lp", ":Leet list<cr>", desc = "Leetcode List" },
+    { ft = leet_arg, "<leader>lx", ":Leet reset<cr>", desc = "Leetcode Reset" },
+    { ft = leet_arg, "<leader>lh", ":Leet last_submit<cr>", desc = "Leetcode Last Submit" },
   },
 }
