637e417d141240e05d8a7b9a769a6c0b3f7580ea
gogamid <36050790+gogamid@users.noreply.github.com>
2025-12-25 23:58:19 +0100

    up

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,14 +3,9 @@ return {
     "mason-org/mason.nvim",
     opts = {
       log_level = vim.log.levels.DEBUG,
-      -- install_root_dir = os.getenv("HOME") .. "/.local/bin/",
     },
     keys = {
       { "<leader>M", "<cmd>Mason<cr>", desc = "Mason Menu" },
     },
-    -- config = function(_, opts)
-    --   require("mason").setup(opts)
-    --   -- vim.cmd[[MasonInstall buf bash-language-server gofumpt goimports gopls json-lsp lua-language-server prettier shfmt sqlls sqls stylua taplo vtsls vue-language-server yaml-language-server yamlfmt]]
-    -- end,
   },
 }
