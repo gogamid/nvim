d2009a79b57aa1f1f4c2c8d2d372dc907077a936
gogamid <36050790+gogamid@users.noreply.github.com>
2025-12-25 16:18:48 +0100

    clean

--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -1,31 +1,5 @@
 return {
   {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {
-      heading = {
-        sign = false,
-      },
-    },
-    ft = { "markdown", "norg", "rmd", "org", "codecompanion", "Avante" },
-    config = function(_, opts)
-      require("render-markdown").setup(opts)
-      Snacks.toggle({
-        name = "Render Markdown",
-        get = function()
-          return require("render-markdown.state").enabled
-        end,
-        set = function(enabled)
-          local m = require("render-markdown")
-          if enabled then
-            m.enable()
-          else
-            m.disable()
-          end
-        end,
-      }):map("<leader>um")
-    end,
-  },
-  {
     "obsidian-nvim/obsidian.nvim",
     version = "*",
     lazy = true,
@@ -97,6 +71,18 @@ return {
     },
     keys = {
       { "<leader>n", "<cmd>GlobalNote<cr>", desc = "Global note" },
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        sign = false,
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion", "Avante" },
+    keys = {
+      { "<leader>um", ":RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
     },
   },
 }
