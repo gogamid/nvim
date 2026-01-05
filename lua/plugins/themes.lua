b8cdb89d77ff44d91d5c04cc47043484dda19784
gogamid <36050790+gogamid@users.noreply.github.com>
2026-01-05 13:00:34 +0100

    remove catpuccin

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,8 +1,3 @@
-local function set_transparent_background(val)
-  local opts = require("catppuccin").options
-  opts.transparent_background = val
-  require("catppuccin").setup(opts)
-end
 return {
   {
     "ribru17/bamboo.nvim",
@@ -23,7 +18,7 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.g.zenbones = { italic_strings = false, transparent_background = true }
+      vim.g.zenbones = { italic_strings = false, transparent_background = false }
       vim.cmd("colorscheme zenbones")
 
       local lush = require("lush")
@@ -49,84 +44,6 @@ return {
       set_light_mode = function()
         vim.o.background = "light"
       end,
-    },
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      color_overrides = {
-        -- frappe = {
-        --   rosewater = "#efd3cd",
-        --   flamingo = "#ecc8c8",
-        --   pink = "#e9c9e0",
-        --   mauve = "#c4aee1",
-        --   red = "#df99aa",
-        --   maroon = "#e2a5ae",
-        --   peach = "#ebb99b",
-        --   yellow = "#eee3cd",
-        --   green = "#6d9c6a",
-        --   teal = "#a5ded4",
-        --   sky = "#9dd8e3",
-        --   sapphire = "#89c4e0",
-        --   blue = "#96b9f1",
-        --   lavender = "#b6bff5",
-        --   text = "#afb4bc",
-        --   subtext1 = "#bac2de",
-        --   subtext0 = "#a6adc8",
-        --   overlay2 = "#9399b2",
-        --   overlay1 = "#7f849c",
-        --   overlay0 = "#6c7086",
-        --   surface2 = "#585b70",
-        --   surface1 = "#45475a",
-        --   surface0 = "#313244",
-        --   base = "#1b1b20",
-        --   mantle = "#19191f",
-        --   crust = "#ffffff",
-        -- },
-        -- latte = {
-        --   rosewater = "#dc8a78",
-        --   flamingo = "#dd7878",
-        --   pink = "#ea76cb",
-        --   mauve = "#8839ef",
-        --   red = "#d20f39",
-        --   maroon = "#e64553",
-        --   peach = "#fe640b",
-        --   yellow = "#df8e1d",
-        --   green = "#40a02b",
-        --   teal = "#179299",
-        --   sky = "#04a5e5",
-        --   sapphire = "#209fb5",
-        --   blue = "#1e66f5",
-        --   lavender = "#7287fd",
-        --   text = "#4c4f69",
-        --   subtext1 = "#5c5f77",
-        --   subtext0 = "#6c6f85",
-        --   overlay2 = "#7c7f93",
-        --   overlay1 = "#8c8fa1",
-        --   overlay0 = "#9ca0b0",
-        --   surface2 = "#acb0be",
-        --   surface1 = "#bcc0cc",
-        --   surface0 = "#ccd0da",
-        --   base = "#eff1f5",
-        --   mantle = "#e6e9ef",
-        --   crust = "#dce0e8",
-        -- },
-      },
-      custom_highlights = function(c)
-        return {
-          Comment = { fg = c.overlay0 },
-        }
-      end,
-      highlight_overrides = {
-        mocha = function(c)
-          return {
-            SnacksIndentScope = { fg = c.overlay1 },
-            LspCodeLens = { fg = c.surface1 },
-            LspCodeLensSeparator = { fg = c.surface2 },
-          }
-        end,
-      },
     },
   },
 }
