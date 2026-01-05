b6265ff0a27838d57e2d3db22808810a6a171957
gogamid <36050790+gogamid@users.noreply.github.com>
2026-01-05 13:27:22 +0100

    up

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,9 +1,6 @@
--- local is_ssh = os.getenv("SSH_CONNECTION") ~= nil
-local is_ssh = false --webssh supports icons
-
 return {
   { "nvim-tree/nvim-web-devicons", enabled = not is_ssh },
   { "nvim-mini/mini.icons", opts = {
-    style = is_ssh and "ascii" or "glyph",
+    style = vim.g.icons_enabled and "glyph" or "ascii",
   } },
 }
