40c692b9b581ba04d9d8176ca84bca1d54319a77
gogamid <36050790+gogamid@users.noreply.github.com>
2026-01-05 13:28:39 +0100

    up

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,5 +1,5 @@
 return {
-  { "nvim-tree/nvim-web-devicons", enabled = not is_ssh },
+  { "nvim-tree/nvim-web-devicons", enabled = vim.g.icons_enabled },
   { "nvim-mini/mini.icons", opts = {
     style = vim.g.icons_enabled and "glyph" or "ascii",
   } },
