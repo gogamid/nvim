164db0d3442a302a22968b7f1feb3ad8bad9f79e
gogamid <36050790+gogamid@users.noreply.github.com>
2026-01-16 11:25:01 +0100

    light theme fix

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,3 +1,19 @@
+local function mod_theme()
+  vim.g.zenbones = { italic_strings = false, transparent_background = false }
+  vim.cmd("colorscheme zenbones")
+
+  local lush = require("lush")
+  local zb = require("zenbones")
+
+  local stringColor = vim.o.background == "light" and "#4F6C30" or "#819B69"
+  local specs = lush.parse(function()
+    return {
+      String({ zb.String, fg = stringColor }),
+    }
+  end)
+  lush.apply(lush.compile(specs))
+end
+
 return {
   {
     "mcchrish/zenbones.nvim",
@@ -5,18 +21,7 @@ return {
     lazy = false,
     priority = 1000,
     init = function()
-      vim.g.zenbones = { italic_strings = false, transparent_background = false }
-      vim.cmd("colorscheme zenbones")
-
-      local lush = require("lush")
-      local zb = require("zenbones")
-
-      local specs = lush.parse(function()
-        return {
-          String({ zb.String, fg = "#819B69" }),
-        }
-      end)
-      lush.apply(lush.compile(specs))
+      mod_theme()
     end,
   },
   {
@@ -25,9 +30,11 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
+        mod_theme()
       end,
       set_light_mode = function()
         vim.o.background = "light"
+        mod_theme()
       end,
     },
   },
