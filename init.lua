42a889a465da9f282ae531f67531fe0becb4335d
gogamid <36050790+gogamid@users.noreply.github.com>
2026-01-26 08:13:39 +0100

    up

--- init.lua
+++ init.lua
@@ -1,4 +1,10 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
--- require("config.lazy")
+vim.g.minimal = false
+
+if vim.g.minimal then
+  require("minimal")
+else
+  require("config.options")
+  require("config.keymaps")
+  require("config.autocmds")
+  require("config.lazy")
+end
