4f9a6068632e619b2e73f1c01036d48ae36bf649
Imron Gamidli <gamidli@MacBook-Air-von-Imron.local>
2026-02-07 08:14:28 +0100

    remove minimal

--- init.lua
+++ init.lua
@@ -1,10 +1,4 @@
-vim.g.minimal = false
-
-if vim.g.minimal then
-  require("minimal")
-else
-  require("config.options")
-  require("config.keymaps")
-  require("config.autocmds")
-  require("config.lazy")
-end
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+require("config.lazy")
