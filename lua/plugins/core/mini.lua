f044f29aa6e7a607b8df58f9aa72350c50176667
gogamid <gamidli@MacBook-Air-von-Imron.local>
2026-02-28 23:29:16 +0100

    up

--- lua/plugins/core/mini.lua
+++ lua/plugins/core/mini.lua
@@ -1,3 +1,6 @@
+local filter_hide = function(fs_entry)
+  return not vim.startswith(fs_entry.name, ".")
+end
 return {
   {
     "nvim-mini/mini.nvim",
@@ -46,6 +49,9 @@ return {
         -- replaces netrw
         use_as_default_explorer = true,
       },
+      content = {
+        filter = filter_hide,
+      },
     },
     keys = {
       {
@@ -59,7 +65,7 @@ return {
     config = function(_, opts)
       require("mini.files").setup(opts)
 
-      local show_dotfiles = true
+      local show_dotfiles = false
       local filter_show = function(fs_entry)
         return true
       end
@@ -73,15 +79,6 @@ return {
         require("mini.files").refresh({ content = { filter = new_filter } })
       end
 
-      local files_set_cwd = function()
-        local cur_entry_path = MiniFiles.get_fs_entry().path
-        local cur_directory = vim.fs.dirname(cur_entry_path)
-        if cur_directory ~= nil then
-          vim.fn.chdir(cur_directory)
-        end
-      end
-
-      -- Yank in register full path of entry under cursor
       local yank_path = function()
         local path = (MiniFiles.get_fs_entry() or {}).path
         if path == nil then
@@ -95,6 +92,20 @@ return {
         vim.ui.open(MiniFiles.get_fs_entry().path)
       end
 
+      local untar = function()
+        local entry = MiniFiles.get_fs_entry()
+        if entry == nil then
+          return
+        end
+        local path = entry.path
+        if vim.fn.executable("tar") == 1 then
+          local path_dir = vim.fn.fnamemodify(path, ":h")
+          vim.fn.system({ "tar", "-xf", path, "-C", path_dir })
+          vim.notify("Untarred " .. path)
+          require("mini.files").refresh()
+        end
+      end
+
       local show_in_finder = function()
         vim.fn.system({ "open", "-R", MiniFiles.get_fs_entry().path })
       end
@@ -102,24 +113,13 @@ return {
       vim.api.nvim_create_autocmd("User", {
         pattern = "MiniFilesBufferCreate",
         callback = function(args)
-          local buf_id = args.data.buf_id
+          local buf = args.data.buf_id
 
-          vim.keymap.set(
-            "n",
-            opts.mappings and opts.mappings.toggle_hidden or "g.",
-            toggle_dotfiles,
-            { buffer = buf_id, desc = "Toggle hidden files" }
-          )
-
-          vim.keymap.set(
-            "n",
-            opts.mappings and opts.mappings.change_cwd or "gc",
-            files_set_cwd,
-            { buffer = args.data.buf_id, desc = "Set cwd" }
-          )
-          vim.keymap.set("n", "go", ui_open, { buffer = args.data.buf_id, desc = "OS open" })
-          vim.keymap.set("n", "gf", show_in_finder, { buffer = args.data.buf_id, desc = "Show in finder" })
-          vim.keymap.set("n", "gy", yank_path, { buffer = args.data.buf_id, desc = "Yank path" })
+          vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf, desc = "Toggle hidden files" })
+          vim.keymap.set("n", "go", ui_open, { buffer = buf, desc = "OS open" })
+          vim.keymap.set("n", "gf", show_in_finder, { buffer = buf, desc = "Show in finder" })
+          vim.keymap.set("n", "gy", yank_path, { buffer = buf, desc = "Yank path" })
+          vim.keymap.set("n", "<C-x>", untar, { buffer = buf, desc = "Untar" })
         end,
       })
     end,
