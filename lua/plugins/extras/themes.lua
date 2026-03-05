862b82604310a4f0951b035459dc6c0be100de5b
gogamid <36050790+gogamid@users.noreply.github.com>
2026-03-05 22:38:52 +0100

    up

--- lua/plugins/extras/themes.lua
+++ lua/plugins/extras/themes.lua
@@ -1,55 +1,52 @@
-local function mod_theme()
-  local zenversion = "zenbones"
-  vim.g.zenbones = { italic_strings = true, transparent_background = false, darkness = "stark" }
-  vim.cmd("colorscheme " .. zenversion)
-
-  local lush = require("lush")
-  local zb = require("zenbones")
-
-  local stringColor = vim.o.background == "light" and "#4F6C30" or "#819B69"
-  local specs = lush.parse(function()
-    return {
-      String({ zb.String, fg = stringColor }),
-    }
-  end)
-  lush.apply(lush.compile(specs))
-
-  vim.api.nvim_set_hl(0, "DebugPrintLine", { link = "DiagnosticHint" })
-end
-
 return {
-  {
-    "oskarnurm/koda.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- require("koda").setup({ transparent = true })
-      vim.cmd("colorscheme koda")
-    end,
-  },
-  {
-    "mcchrish/zenbones.nvim",
-    enabled = false,
-    dependencies = "rktjmp/lush.nvim",
-    lazy = false,
-    priority = 1000,
-    init = function()
-      mod_theme()
-    end,
-  },
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
-        -- mod_theme()
       end,
       set_light_mode = function()
         vim.o.background = "light"
-        -- mod_theme()
       end,
     },
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.zenbones = { italic_strings = true, transparent_background = false, darkness = "stark" }
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "zenbones",
+        callback = function()
+          local lush = require("lush")
+          local zb = require("zenbones")
+          local stringColor = vim.o.background == "light" and "#4F6C30" or "#819B69"
+          local specs = lush.parse(function()
+            return {
+              String({ zb.String, fg = stringColor }),
+              SnacksPicker({ bg = "NONE" }),
+              NormalFloat({ bg = "NONE" }),
+              FloatBorder({ zb.FlotBorder, bg = "NONE" }),
+            }
+          end)
+          lush.apply(lush.compile(specs))
+          vim.api.nvim_set_hl(0, "DebugPrintLine", { link = "DiagnosticHint" })
+        end,
+      })
+      vim.cmd.colorscheme("zenbones")
+    end,
+  },
+  {
+    "oskarnurm/koda.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- require("koda").setup({ transparent = true })
+      vim.cmd("colorscheme koda")
+    end,
   },
   {
     "ribru17/bamboo.nvim",
