a25b71c22516ad67c0095a576fcf8f0d97c1d12d
gogamid <36050790+gogamid@users.noreply.github.com>
2026-08-30 13:32:13 +0200

    background

--- lua/plugins/extras/themes.lua
+++ lua/plugins/extras/themes.lua
@@ -5,9 +5,17 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.o.background = "dark"
+        local img = vim.fn.stdpath("config") .. "/backgrounds/flexoki-dark-orb.png"
+        vim.fn.system(
+          'osascript -e \'tell application "System Events" to tell every desktop to set picture to "' .. img .. "\"'"
+        )
       end,
       set_light_mode = function()
         vim.o.background = "light"
+        local img = vim.fn.stdpath("config") .. "/backgrounds/flexoki-light-orb.png"
+        vim.fn.system(
+          'osascript -e \'tell application "System Events" to tell every desktop to set picture to "' .. img .. "\"'"
+        )
       end,
     },
   },
@@ -17,7 +25,8 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.g.zenbones = { italic_strings = true, transparent_background = false, darkness = "stark" }
+      vim.g.zenbones = { italic_strings = true, transparent_background = true, darkness = "warm", lightness = "dim" }
+
       vim.api.nvim_create_autocmd("ColorScheme", {
         pattern = "zenbones",
         callback = function()
@@ -30,41 +39,13 @@ return {
               SnacksPicker({ bg = "NONE" }),
               NormalFloat({ bg = "NONE" }),
               FloatBorder({ zb.FloatBorder, bg = "NONE" }),
+              DebugPrintLine({ zb.DiagnosticHint, bg = "NONE" }),
             }
           end)
           lush.apply(lush.compile(specs))
-          vim.api.nvim_set_hl(0, "DebugPrintLine", { link = "DiagnosticHint" })
         end,
       })
       vim.cmd.colorscheme("zenbones")
-    end,
-  },
-  {
-    "oskarnurm/koda.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- require("koda").setup({ transparent = true })
-      vim.cmd("colorscheme koda")
-    end,
-  },
-  {
-    "ribru17/bamboo.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function(_, opts)
-      require("bamboo").setup(opts)
-    end,
-  },
-  {
-    "slugbyte/lackluster.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    init = function()
-      vim.cmd("colorscheme lackluster")
     end,
   },
 }
