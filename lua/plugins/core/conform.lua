7d998908d6122023af1496f6d48c0af957a39023
gogamid <36050790+gogamid@users.noreply.github.com>
2026-09-20 09:05:24 +0200

    removve org imports

--- lua/plugins/core/conform.lua
+++ lua/plugins/core/conform.lua
@@ -1,32 +1,5 @@
+local disabled_format_ft = { "sql", "bigfile" }
 local jsFormatter = { "prettier", lsp_format = "never" }
-
-local function organize_go_imports(bufnr)
-  local client = vim.lsp.get_clients({ name = "gopls", bufnr = bufnr })[1]
-  if not client then
-    return
-  end
-
-  local params = vim.lsp.util.make_range_params(0, client.offset_encoding or "utf-8")
-  params.context = { only = { "source.organizeImports" }, diagnostics = {} }
-
-  local result = client:request_sync("textDocument/codeAction", params, 3000, bufnr)
-  if not result or result.err or not result.result or vim.tbl_isempty(result.result) then
-    return
-  end
-
-  for _, action in ipairs(result.result) do
-    if action.kind == "source.organizeImports" then
-      vim.notify("Organizing imports...")
-      if action.edit then
-        vim.lsp.util.apply_workspace_edit(action.edit, client.offset_encoding or "utf-8")
-      end
-      if action.command then
-        client:request_sync("workspace/executeCommand", action.command, 3000, bufnr)
-      end
-      return
-    end
-  end
-end
 
 return {
   {
@@ -104,19 +77,13 @@ return {
         return gaf == nil or gaf
       end
 
-      local always_disabled_fts = { "sql", "bigfile" }
-
       vim.api.nvim_create_autocmd("BufWritePre", {
         callback = function(event)
-          local filetype = vim.bo[event.buf].filetype
-          if vim.tbl_contains(always_disabled_fts, filetype) then
+          if vim.tbl_contains(disabled_format_ft, vim.bo[event.buf].filetype) then
             return
           end
           if autoformat_enabled(event.buf) then
             require("conform").format({ bufnr = event.buf, async = false, timeout_ms = 3000 })
-            -- if filetype == "go" then
-            --   organize_go_imports(event.buf)
-            -- end
           end
         end,
       })
